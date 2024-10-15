434fdc1f8a81881ce110b283df55cc658ff321af
Justin Garner <justin2013pdx@gmail.com>
2024-10-15 01:00:37 -0400

    Frontend complete

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import winkIcon from "../public/assets/ph_smiley-wink-fill.png";
 import hattlLogo from "../public/assets/LOGO MAIN 1.png";
 import phoness from "../public/assets/Frame 34427.png";
 import laptopss from "../public/assets/Macbook Air (2022).png";
+import backgroundblobs from "../public/assets/Group 49.png";
 import './App.css';
 
 function App() {
@@ -15,117 +16,120 @@ function App() {
   const screenWidth = screen.width;
 
   return (
-    <div className='w-full h-fit bg-[#121114] pb-10 px-5'>
-      <nav className='flex justify-between items-center pt-7'>
-        <img className='h-10' src={hattlLogo} alt="" />
+    <div className='bg-[#121114] h-fit relative overflow-hidden'>
+      <img className='absolute z-10 w-full object-cover opacity-70' src={backgroundblobs} alt="" />
+      <div className='w-full h-fit pb-6 px-5 z-20 relative'>
+        <nav className='flex justify-between items-center pt-7'>
+          <img className='h-10' src={hattlLogo} alt="" />
 
-        <div className='flex items-center gap-4'>
-          <img className='h-10' src={questionIcon} alt="Question Icon" />
+          <div className='flex items-center gap-4'>
+            <img className='h-10' src={questionIcon} alt="Question Icon" />
 
-          <button className='bg-white flex py-2.5 px-4 rounded-full gap-2.5 font-light'>
-            <img className='h-6' src={winkIcon} alt="" />
-            Join beta
-          </button>
-        </div>
-      </nav>
-
-      <div className='flex flex-wrap h-fit'>
-        <div className='md:w-1/2 md:pt-10 md:pl-8'>
-          <h1 className='text-white mt-12 text-4xl md:text-5xl font-semibold'>
-            6 free months<br /> <span className='text-gray-400'>for our first users</span>
-          </h1>
-
-          <p className='md:text-lg mt-6 text-gray-400'>
-            We’ll launch a platform with a huge number of features that you will definitely like.
-            <span className='block pt-5 md:pt-2'>
-              And now you can become the very first user and get a god mode absolutely free.
-            </span>
-          </p>
-
-          {screenWidth > 1024 ? (
-            <div className=''>
-              <div className='mt-8 h-14 flex items-center w-full border-b-4 border-gray-400'>
-                <input className='bg-transparent h-full grow text-lg text-white' type="text" placeholder='Your email' name="" id="" />
-                <img className='h-10' src="https://img.icons8.com/ios-filled/100/ffffff/long-arrow-right.png" alt="" />
-              </div>
-
-              <p className='mt-7 text-gray-400'>
-                We’re really honest guys and promise not to send you ads. We will write only once when our platform is ready.
-              </p>
-            </div>
-          ) : (
-            <p></p>
-          )}
-        </div>
-        
-        {screenWidth > 1024 ? (
-          <div className='w-1/2 flex justify-end'>
-            <img className='mt-8 w-4/5' src={laptopss} alt="" />
+            <button className='bg-white flex py-2.5 px-4 rounded-full gap-2.5 font-light'>
+              <img className='h-6' src={winkIcon} alt="" />
+              Join beta
+            </button>
           </div>
-        ) : (
-          <img className='mt-8 w-full md:w-1/2' src={phoness} alt="" />
-        )}
+        </nav>
 
-          {screenWidth > 1024 ? (
-            <p></p>
-          ) : (
-            <div className='md:w-[37%] md:relative md:-top-40 md:pl-8'>
-            <div className='mt-8 h-14 flex items-center w-full border-b-4 border-gray-400'>
-              <input className='bg-transparent h-full grow text-lg text-white' type="text" placeholder='Your email' name="" id="" />
+        <div className='flex flex-wrap h-fit'>
+          <div className='md:w-1/2 md:pt-10 md:pl-8'>
+            <h1 className='text-white mt-12 text-4xl md:text-5xl font-semibold'>
+              6 free months<br /> <span className='text-gray-400'>for our first users</span>
+            </h1>
 
-              <img className='h-10' src="https://img.icons8.com/ios-filled/100/ffffff/long-arrow-right.png" alt="" />
-            </div>
-
-            <p className='mt-7 text-gray-400'>
-              We’re really honest guys and promise not to send you ads. We will write only once when our platform is ready.
+            <p className='md:text-lg mt-6 text-gray-400'>
+              We’ll launch a platform with a huge number of features that you will definitely like.
+              <span className='block pt-5 md:pt-2'>
+                And now you can become the very first user and get a god mode absolutely free.
+              </span>
             </p>
+
+            {screenWidth > 768 ? (
+              <div className=''>
+                <div className='mt-8 h-14 flex items-center w-full border-b-4 border-gray-400'>
+                  <input className='bg-transparent h-full grow text-lg text-white' type="text" placeholder='Your email' name="" id="" />
+                  <img className='h-10' src="https://img.icons8.com/ios-filled/100/ffffff/long-arrow-right.png" alt="" />
+                </div>
+
+                <p className='mt-7 text-gray-400'>
+                  We’re really honest guys and promise not to send you ads. We will write only once when our platform is ready.
+                </p>
+              </div>
+            ) : (
+              <p></p>
+            )}
           </div>
+          
+          {screenWidth > 768 ? (
+            <div className='w-1/2 flex justify-end'>
+              <img className='mt-8 w-4/5' src={laptopss} alt="" />
+            </div>
+          ) : (
+            <img className='mt-8 w-full md:w-1/2' src={phoness} alt="" />
           )}
-      </div>
 
-      <h2 className='mt-8 md:mt-0 text-white text-2xl md:text-3xl font-medium md:pl-8'>Why it's cool?</h2>
+            {screenWidth > 768 ? (
+              <p></p>
+            ) : (
+              <div className=''>
+                <div className='mt-10 md:mt-8 h-14 flex items-center w-full border-b-4 border-gray-400'>
+                  <input className='bg-transparent h-full grow text-lg text-white' type="text" placeholder='Your email' name="" id="" />
 
-      <div className='mt-6 flex flex-col md:flex-row gap-10 md:px-8'>
-        <div className=''>
-          <h3 className='mb-3 text-white text-xl md:text-2xl font-medium'>Games</h3>
+                  <img className='h-10' src="https://img.icons8.com/ios-filled/100/ffffff/long-arrow-right.png" alt="" />
+                </div>
 
-          <p className='text-white text-sm md:text-base'>
-            Love Spotify? Now imagine that we can just as well match games to fit your taste. We are already doing it.
-          </p>
-
-          <button className='mt-5 bg-[#f1753d] text-white text-xl font-medium px-9 py-2 rounded-full'>gameplay-based engine</button>
+                <p className='mt-7 text-gray-400'>
+                  We’re really honest guys and promise not to send you ads. We will write only once when our platform is ready.
+                </p>
+              </div>
+            )}
         </div>
 
-        <div className=''>
-          <h3 className='mb-3 text-white text-xl md:text-2xl font-medium'>Community</h3>
+        <h2 className='mt-14 md:mt-11 text-white text-2xl md:text-3xl font-medium md:pl-8'>Why it's cool?</h2>
 
-          <p className='text-white text-sm md:text-base'>
-            Hattl also shows how similar your game tastes are with your friends. And hattl helps find new teammates.
-          </p>
+        <div className='mt-7 md:mt-8 flex flex-col md:flex-row gap-10 md:px-8'>
+          <div className=''>
+            <h3 className='mb-3 text-white text-xl md:text-2xl font-medium'>Games</h3>
 
-          <button className='mt-5 bg-[#5253f1] text-white text-xl font-medium px-9 py-2 rounded-full'>teammates and friends</button>
+            <p className='text-white text-sm md:text-base'>
+              Love Spotify? Now imagine that we can just as well match games to fit your taste. We are already doing it.
+            </p>
+
+            <button className='mt-5 bg-[#f1753d] text-white text-xl font-medium px-9 py-2 rounded-full'>gameplay-based engine</button>
+          </div>
+
+          <div className=''>
+            <h3 className='mb-3 text-white text-xl md:text-2xl font-medium'>Community</h3>
+
+            <p className='text-white text-sm md:text-base'>
+              Hattl also shows how similar your game tastes are with your friends. And hattl helps find new teammates.
+            </p>
+
+            <button className='mt-5 bg-[#5253f1] text-white text-xl font-medium px-9 py-2 rounded-full'>teammates and friends</button>
+          </div>
         </div>
+
+        <h1 className='text-white mt-20 md:mt-32 text-4xl md:text-5xl font-semibold md:pl-8'>
+          6 free months<br /> <span className='text-gray-400'>for our first users</span>
+        </h1>
+
+        <div className='mt-8 h-14 flex items-center w-full border-b-4 border-gray-400 md:w-3/5 md:ml-8'>
+          <input className='bg-transparent h-full grow text-lg text-white' type="text" placeholder='Your email' name="" id="" />
+
+          <img className='h-10' src="https://img.icons8.com/ios-filled/100/ffffff/long-arrow-right.png" alt="" />
+        </div>
+
+        <p className='mt-7 text-gray-400 md:pl-8'>
+          We’re really honest guys and promise not to send you ads. <br className='hidden md:block'/>We will write only once when our platform is ready.
+        </p>
+
+        <p className='mt-16 md:mt-20 mb-6 md:mb-14 text-white text-center font-medium px-8'>
+          We are just starting our journey, but we promise that it will be cool!
+        </p>
+
+        <Footer/>
       </div>
-
-      <h1 className='text-white mt-12 text-4xl md:text-5xl font-semibold pl-8'>
-        6 free months<br /> <span className='text-gray-400'>for our first users</span>
-      </h1>
-
-      <div className='mt-8 h-14 flex items-center w-full border-b-4 border-gray-400 w-3/5 ml-8'>
-        <input className='bg-transparent h-full grow text-lg text-white' type="text" placeholder='Your email' name="" id="" />
-
-        <img className='h-10' src="https://img.icons8.com/ios-filled/100/ffffff/long-arrow-right.png" alt="" />
-      </div>
-
-      <p className='mt-7 text-gray-400 pl-8'>
-        We’re really honest guys and promise not to send you ads. <br className='hidden md:block'/>We will write only once when our platform is ready.
-      </p>
-
-      <p className='mt-16 text-white text-center font-medium px-8'>
-        We are just starting our journey, but we promise that it will be cool!
-      </p>
-
-      <Footer/>
     </div>
   )
 }
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBs5lYTaRRPFtFgSct6kVdOhiFzWKHqIYU",
+  authDomain: "hattl-waitlist.firebaseapp.com",
+  projectId: "hattl-waitlist",
+  storageBucket: "hattl-waitlist.appspot.com",
+  messagingSenderId: "1007865695640",
+  appId: "1:1007865695640:web:a3d5115ae4bc653100efed"
+};
+
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore (app);
